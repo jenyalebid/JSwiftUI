5e8aecefaaff2d6644a6de2a78dec5bdb03d234b
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2023-07-31 22:02:01 -0700

    Added ExpandableTextField.

---
+++ Sources/JSwiftUI/Blocks/Modifiers/RequiredViewModifier.swift
@@ -0,0 +1,42 @@
+//
+//  RequiredViewModifier.swift
+//  JSwiftUI
+//
+//  Created by Jenya Lebid on 7/31/23.
+//
+
+import SwiftUI
+
+struct RequiredViewModifier: ViewModifier {
+    
+    var isRequired: Bool
+    var padding: CGFloat
+    
+    init(isRequired: Bool, padding: CGFloat) {
+        self.isRequired = isRequired
+        self.padding = padding
+    }
+
+    func body(content: Content) -> some View {
+        if isRequired {
+            ZStack {
+                content
+                    .padding(padding)
+                RoundedRectangle(cornerRadius: 10)
+                    .strokeBorder(lineWidth: 1)
+                    .foregroundColor(Color(uiColor: .red))
+            }
+        }
+        else {
+            content
+        }
+    }
+}
+
+extension View {
+    
+    func requiredOutline(_ isRequired: Bool, padding: CGFloat = 0) -> some View {
+        modifier(RequiredViewModifier(isRequired: isRequired, padding: padding))
+    }
+}
+
